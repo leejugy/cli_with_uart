cfc1c682590817b42db81ec4b9b695667eb63a83
leejugy <lee09260@naver.com>
2024-05-01 23:23:08 +0900

    ymodem_with_cli

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -75,6 +75,56 @@ uint16_t led1_toggle_time=100;
 uint16_t led2_toggle_time=100;
 bool led1_toggle=false;
 bool led2_toggle=false;
+static const uint16_t crc_table[256] = {
+    0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
+    0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef,
+    0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
+    0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de,
+    0x2462, 0x3443, 0x0420, 0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485,
+    0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
+    0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4,
+    0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc,
+    0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
+    0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b,
+    0x5af5, 0x4ad4, 0x7ab7, 0x6a96, 0x1a71, 0x0a50, 0x3a33, 0x2a12,
+    0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
+    0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41,
+    0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
+    0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
+    0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78,
+    0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f,
+    0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
+    0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e,
+    0x02b1, 0x1290, 0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256,
+    0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
+    0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+    0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e, 0xc71d, 0xd73c,
+    0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
+    0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab,
+    0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x08e1, 0x3882, 0x28a3,
+    0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
+    0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92,
+    0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9,
+    0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
+    0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8,
+    0x6e17, 0x7e36, 0x4e55, 0x5e74, 0x2e93, 0x3eb2, 0x0ed1, 0x1ef0
+};
+
+typedef enum{
+	SOH=0x01,
+	STX=0x02,
+	EOT=0x04,
+	ACK=0x06,
+	NAK=0x15,
+	CAN=0x18,
+} ymodem_protocol_name;
+
+uint8_t rx_ymodem[1024+5];
+uint8_t rx_1bit;
+uint16_t rx_index=0;
+uint8_t send_buffer[send_buffer_length];
+bool uart1_send_flag = false;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -85,11 +135,145 @@ extern HAL_StatusTypeDef UART_Start_Receive_DMA(UART_HandleTypeDef *huart, uint8
 bool is_same_word(uint8_t *buf,uint8_t *compare,uint8_t start_compare);
 keytype is_command(uint8_t *buf,uint8_t i);
 void cli_start(uint8_t i,keytype key_input);
+void ymodem_transmit();
+void uart_send_1bit(uint8_t uart_num,uint8_t *send_1bit_buffer);
+void uart_send(uint8_t uart_num, char *fmt,...);
+
+
+void uart_send_1bit(uint8_t uart_num,uint8_t *send_1bit_buffer){
+	if(uart_num==1){
+		HAL_UART_Transmit(&huart1,send_1bit_buffer,1,100);
+	}
+	else if(uart_num==2){
+		HAL_UART_Transmit(&huart2,send_1bit_buffer,1,100);
+	}
+}
+
+bool calculate_crc16(uint8_t *data, int length, bool *loop_do) {
+    uint16_t crc = 0;
+    for (int i = 0; i < length; i++) {
+        crc = (crc << 8) ^ crc_table[((crc >> 8) ^ data[i]) & 0xFF];
+    }
+    if(crc!=0){
+    	uart_send(2,"CRC is incorrect!\n");
+    	*loop_do=false;
+    	return false;
+    }
+    return true;
+}
+void ymodem_transmit(){
+	HAL_UART_DMAStop(&huart1);
+	UART_Start_Receive_DMA(&huart1,&rx_1bit,1);
+
+	HAL_Delay(100);
+	bool send_c_flag=true;
+	bool first_send_eot=true;
+	bool loop_do=true;
+
+	uint8_t _1bit_buffer;
+	uint32_t file_size=0;
+	uint8_t file_index;
+	uint8_t file_name[64];
+	memset(file_name,0,64);
+	memset(rx_ymodem,0,1029);
+	rx_index=0;
+
+	uart_send(1,"\n");
+	while(loop_do){
+		uart1_send_flag=true;
+		if(send_c_flag){
+			HAL_Delay(100);
+			_1bit_buffer='C';
+			uart_send_1bit(1,&_1bit_buffer);
+			_1bit_buffer='C';
+			uart_send_1bit(1,&_1bit_buffer);
+		}
+		if(rx_ymodem[0]==SOH){
+			HAL_Delay(10);
+			for(file_index=3;file_index<128;file_index++){
+				file_name[file_index-3]=rx_ymodem[file_index];
+				if(rx_ymodem[file_index]==0){
+					file_index++;
+					break;
+				}
+			}
+			for(;file_index<128;file_index++){
+				if(rx_ymodem[file_index]==' '){
+					break;
+				}
+				else{
+					file_size=(rx_ymodem[file_index]-48)+file_size*10;
+				}
+			}
+			calculate_crc16(rx_ymodem+1027, 2, &loop_do);
+
+			memset(rx_ymodem,0,1029);
+			rx_index=0;
+
+			_1bit_buffer=ACK;
+			uart_send_1bit(1,&_1bit_buffer);
+			_1bit_buffer='C';
+			uart_send_1bit(1,&_1bit_buffer);
+			send_c_flag=false;
+		}
+		else if(rx_1bit==EOT && first_send_eot){
+			_1bit_buffer=NAK;
+			uart_send_1bit(1,&_1bit_buffer);
+			first_send_eot=false;
+		}
+		else if(rx_1bit==EOT && !first_send_eot){
+			first_send_eot=true;
+			_1bit_buffer=ACK;
+			uart_send_1bit(1,&_1bit_buffer);
+			_1bit_buffer='C';
+			uart_send_1bit(1,&_1bit_buffer);
+
+			_1bit_buffer=ACK;
+			uart_send_1bit(1,&_1bit_buffer);
+			_1bit_buffer='C';
+			uart_send_1bit(1,&_1bit_buffer);
+
+			loop_do=false;
+			uart1_send_flag=false;
+			rx_1bit=0;
+
+			uart_send(2,"\n+++++++++++++++++++++++++++++++++++++++++\n");
+			uart_send(2,"\nsend_complete!\n");
+			uart_send(2,"INFO)\nfile name : ");
+			uart_send(2,"%s",file_name);
+			uart_send(2,"\n");
+			uart_send(2,"file size : ");
+			uart_send(2,"%lu",file_size);
+			uart_send(2,"\n");
+			HAL_UART_DMAStop(&huart1);
+			UART_Start_Receive_DMA(&huart1,&command_tx[0],5);;
+		}
+		else if(rx_ymodem[0]==STX){
+			HAL_Delay(10);
+			calculate_crc16(rx_ymodem+1027, 2, &loop_do);
+			/*buffer send_occur*/
+			for(uint16_t i=3;i<=1026;i++){
+				uart_send_1bit(2,&rx_ymodem[i]);
+			}
+			memset(rx_ymodem,0,1029);
+			rx_index=0;
+			_1bit_buffer=ACK;
+			uart_send_1bit(1,&_1bit_buffer);
+			send_c_flag=false;
+		  }
+	}
+}
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-	if(huart->Instance==huart1.Instance){
-		HAL_UART_Receive_DMA(&huart1,&command_tx[0],5);
+	if(huart->Instance==USART1){
+		if(uart1_send_flag){
+			rx_ymodem[rx_index++]=rx_1bit;
+			HAL_UART_Receive_DMA(&huart1,&rx_1bit,1);
+		}
+		else{
+			HAL_UART_Receive_DMA(&huart1,&command_tx[0],5);
+		}
 	}
 	UNUSED(huart);
 }
@@ -107,6 +291,7 @@ void uart_send(uint8_t uart_num, char *fmt,...){
 	memset(send_buffer,0,send_buffer_length);
 	va_end(arg);
 }
+
 keytype is_command(uint8_t *buf,uint8_t i){
 	keytype type_key = KEY_NONE;
 	switch (buf[i]){
@@ -190,6 +375,7 @@ void command_input(uint8_t *buf){
 		uart_send(1,"4. adc read : read adc voltage val, where reference voltage,");
 		uart_send(1,"is 3.3V.\n");
 		uart_send(1,"5. clear : clear terminal.\n");
+		uart_send(1,"6. ymodem transmit : transmit data with ymodem.\n");
 		uart_send(1,"\n---------------------------------------------------");
 		uart_send(1,"----------------\n\n");
 		existing_command = true;
@@ -201,6 +387,10 @@ void command_input(uint8_t *buf){
 	}
 	else if(is_same_word(buf,(uint8_t *)"clear",0)){
 		uart_send(1,"\x1B[2J");
+		existing_command = true;
+	}
+	else if(is_same_word(buf,(uint8_t *)"ymodem transmit",0)){
+		ymodem_transmit();
 		existing_command = true;
 	}
 	else if(is_same_word(buf,(uint8_t *)"adc read",0)){
@@ -404,6 +594,7 @@ void cli_start(uint8_t i,keytype key_input){
 		default:
 			break;
 	}
+	/*
 	uart_send(2,"\x1B[2J");
 	for (int8_t j=0;j<3;j++){
 		uart_send(2,"%d. buffer val:%s\n",j,data[j].command_buffer);
@@ -411,6 +602,7 @@ void cli_start(uint8_t i,keytype key_input){
 	uart_send(2,"\n\n\n");
 	uart_send(2,"num %d command buffer val:%s\n",data_num,data[data_num].command_buffer);
 	uart_send(2,"command index:%d cursor index:%d\n",data[data_num].command_index,data[data_num].cursor_index);
+	*/
 }
 /* USER CODE END PFP */
 
